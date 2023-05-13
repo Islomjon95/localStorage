aeb78f7b313d3bcd3f53a02d0245b33c5625e088
Islom_dev <inogomovislom@gmail.com>
2023-05-13 16:05:16 +0500

    birinchi chiziqgacha turgan komentga olingan kodlar, lokal storage yozish o'qish va o'chirish uchun ishlatilindi

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 // setItem
 // clear()
 
+// const ism = "Diyor"
+
+// localStorage.setItem("name" , ism)
+
+
+// const getName = localStorage.getItem("name")
+
+// console.log(getName)
+
+// localStorage.clear()
+
